daa1424ababf52b713a534fa6754f1bd350ac57e
ADOUANECylia <55532188+ADOUANECylia@users.noreply.github.com>
2021-07-24 18:36:08 +0200

    Update index.php

--- Test Epitech/business/index.php
+++ Test Epitech/business/index.php
@@ -65,7 +65,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>PHPJabbers.com | Free Blog Website Template</title>
+        <title>Business Cards Manager</title>
 
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -99,7 +99,7 @@
                             </div>
                         </div>
                         <div class="col-md-5">
-                            <img src="img/about-1-720x480.jpg" class="img-fluid" alt="">
+                           <!-- <img src="img/about-1-720x480.jpg" class="img-fluid" alt=""> -->
                         </div>
                     </div>
                 </div>
@@ -112,7 +112,7 @@
                     <div class="col-md-5">
                         <div class="about-veno">
                             <div class="logo">
-                                <img src="img/footer_logo.png" alt="Venue Logo">
+                                <!--<img src="img/footer_logo.png" alt="Venue Logo"> -->
                             </div>
                             <p>Mauris sit amet quam congue, pulvinar urna et, congue diam. Suspendisse eu lorem massa. Integer sit amet posuere tellustea dictumst.</p>
                             <ul class="social-icons">
